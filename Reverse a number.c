bdba15c7b2fba51838f4febe74788e8cc35e4037
dhanushnandhakumar023-dev <dhanushnandhakumar023@gmail.com>
2026-02-14 09:55:32 +0530

    Create Reverse a number.c

---
+++ Reverse a number.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int n,rev = 0,rem;
+    scanf("%d", &n);
+    for(int i=n;i>0;i=i/10) {
+        rem=i%10;
+        rev=rev*10 +rem;
+    }
+    printf("%d",rev);
+    return 0;
+}
