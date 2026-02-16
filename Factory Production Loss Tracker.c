5273d7558362602d3084f1d6f38a6265b0f12899
Dhanush-v18 <dhanushnandhakumar023@gmail.com>
2026-02-16 15:42:04 +0530

    Create Factory Production Loss Tracker.c

---
+++ Factory Production Loss Tracker.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int N, loss, totalLoss = 0, highLossDays = 0;
+    scanf("%d",&N);
+    for(int i=1;i<=N;i++){
+        scanf("%d",&loss);
+        totalLoss += loss;
+        if(loss > 100){
+            highLossDays++;
+        }
+    }
+    printf("Total Loss: %d\n", totalLoss);
+    printf("High Loss Days: %d", highLossDays);
+    return 0;
+}
